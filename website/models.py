f652fff064cbb138b7001896d5fbfd414f2a3f7c
Tim Clemans <realtimclemans@gmail.com>
2020-12-25 14:50:45 -0800

    make model

--- website/models.py
+++ website/models.py
@@ -9,14 +9,9 @@ class Customer(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
     is_suspended = db.Column(db.Boolean(), default=False)
     is_email_address_verified = db.Column(db.Boolean(), default=False)
+    is_identity_verified = db.Column(db.Boolean(), default=False)
     email_address_salt = db.Column(db.String())
     email_address_hash = db.Column(db.String())
-    government_identification_numbers = db.Column(JSON, default={}) # example keys: US_state_ID, US_social_security_number
-
-    def __init__(self, url, result_all, result_no_stop_words):
-        self.url = url
-        self.result_all = result_all
-        self.result_no_stop_words = result_no_stop_words
-
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+    password_salt = db.Column(db.String(), nullable=True)
+    password_hash = db.Column(db.String(), nullable=True)
+    unique_identifiers = db.Column(JSON, default={}) # example keys: US_state_ID_number, US_state_that_issued_ID, US_social_security_number
