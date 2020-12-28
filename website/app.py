56f0f3603a1316b3e275d48617117101d19f49af
Tim Clemans <realtimclemans@gmail.com>
2020-12-28 11:33:51 -0800

    get database working

--- website/app.py
+++ website/app.py
@@ -5,10 +5,11 @@ from form import SignupForm, SigninForm
 import random
 import string
 import re
+import hashlib
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, static_url_path="", static_folder="static")
-
 
 SECRET_KEY = os.urandom(32)
 app.config["SECRET_KEY"] = SECRET_KEY
@@ -19,9 +20,37 @@ app.config["RECAPTCHA_OPTIONS"] = {"theme": "black"}
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+db.app = app
+db.init_app(app)
+migrate = Migrate(app, db)
+
+from sqlalchemy.dialects.postgresql import UUID, JSON
+import uuid
 
 
-def create_verification_token():
+
+class Customer(db.Model):
+    __tablename__ = 'customers'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
+    is_suspended = db.Column(db.Boolean(), default=False)
+    reason_for_suspension = db.Column(db.String(), nullable=True)
+    is_email_address_verified = db.Column(db.Boolean(), default=False)
+    verification_token = db.Column(db.String())
+    is_identity_verified = db.Column(db.Boolean(), default=False)
+    email_address_salt = db.Column(db.String())
+    email_address_hash = db.Column(db.String())
+    password_salt = db.Column(db.String(), nullable=True)
+    password_hash = db.Column(db.String(), nullable=True)
+    unique_identifiers = db.Column(JSON, default={}) # example keys: US_state_ID_number, US_state_that_issued_ID, US_social_security_number
+
+    def __init__(self, email_address_salt, email_address_hash, verification_token):
+        self.email_address_salt = email_address_salt
+        self.email_address_hash = email_address_hash
+        self.verification_token = verification_token
+
+
+def create_random_string():
     letters = string.ascii_lowercase
     return "".join([random.choice(letters) for i in range(200)])
 
@@ -56,28 +85,46 @@ def home():
     signup_form = SignupForm()
     if request.form:
         if signup_form.validate_on_submit():
-            verification_token = create_verification_token()
+            verification_token = create_random_string()
+            salt = create_random_string()
             email_address = request.form.get("email_address")
-
-            if " " in email_address:
+            
+            if "+" in email_address:
                 return render_template(
                     "index.html",
                     signup_form=signup_form,
                     is_plus_sign_in_email=True,
                     sending_error=False,
                     is_captcha_valid=True,
+                    is_email_address_invalid=True
                 )
-            regex = "^[a-z0-9] [\._]?[a-z0-9] [@]\w [.]\w{2,3}$"
+            regex = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
             if not re.search(regex, email_address):
                 return render_template(
                     "index.html",
                     signup_form=signup_form,
                     is_plus_sign_in_email=False,
-                    is_email_address_invalid=True,
+                    is_email_address_invalid=False,
                     sending_error=False,
                     is_captcha_valid=True,
                 )
-
+            result = list(db.engine.execute("SELECT '' FROM customers WHERE encode(digest(email_address_salt || ' %s', 'sha1'), 'hex') = email_address_hash" % (email_address)))
+            if len(list(result)) > 0: # don't send verification and don't notify attacker that this email belongs to a customer
+                return render_template(
+                    "index.html",
+                    signup_form=signup_form,
+                    sending_error=False,
+                    sent_verification=True,
+                    is_captcha_valid=True,
+                    is_email_address_invalid=True
+                )
+            hash_object = hashlib.sha1()
+            hash_object.update(str(salt).encode('utf-8'))
+            hash_object.update(str(' ' + email_address).encode('utf-8'))
+            email_hash = hash_object.hexdigest()
+            customer = Customer(salt, email_hash, verification_token)
+            db.session.add(customer)
+            db.session.commit()
             sending_response = send_verification_email(
                 email_address, verification_token
             )
@@ -87,6 +134,7 @@ def home():
                     signup_form=signup_form,
                     sending_error=True,
                     is_captcha_valid=True,
+                    is_email_address_invalid=True
                 )
 
             return render_template(
@@ -95,12 +143,13 @@ def home():
                 sending_error=False,
                 sent_verification=True,
                 is_captcha_valid=True,
+                is_email_address_invalid=True
             )  # regardless if already verified we claim success because we don't disclose to attackers email addresses
         else:
             return render_template(
-                "index.html", signup_form=signup_form, is_captcha_valid=False
+                "index.html", signup_form=signup_form, is_captcha_valid=False, is_email_address_invalid=True
             )
-    return render_template("index.html", signup_form=signup_form, is_captcha_valid=True)
+    return render_template("index.html", signup_form=signup_form, is_captcha_valid=True, is_email_address_invalid=True)
 
 
 if __name__ == "__main__":
--- website/manage.py
+++ website/manage.py
@@ -1,7 +1,6 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
 from app import app, db
 
 
@@ -11,7 +10,8 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
-
+import models
 
 if __name__ == '__main__':
     manager.run()
+    db.create_all()
