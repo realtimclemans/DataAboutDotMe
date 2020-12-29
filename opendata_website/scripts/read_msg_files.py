4fc1a983b885cba5766c9388297be635ae007f82
Tim Clemans <realtimclemans@gmail.com>
2020-12-28 21:55:42 -0800

    add ds store

--- opendata_website/scripts/read_msg_files.py
+++ opendata_website/scripts/read_msg_files.py
@@ -3,6 +3,8 @@ from os import listdir
 from os.path import isfile, join
 import sys
 
+emails = []
+
 mypath = sys.argv[1]
 
 for fname in [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith('msg')]:
@@ -10,9 +12,15 @@ for fname in [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f)
         msg = Message(msg_file)
 
     # Contents are the plaintext body of the email
-    contents = msg.body
-    print(contents)
+    try:
+        sender_email = msg.sender_email
+    except:
+        sender_email = ''
+    email = {'sender_email': sender_email, 'body': msg.body, 'subject': msg.subject, 'attachment_file_names': [attachment.filename for attachment in msg.attachments]}
+    emails.append(email)
     # Attachments can be read and saved like so
     #first_attachment = msg.attachments[0]
     #with first_attachment.open() as attachment_fp, open(first_attachment.filename, 'wb') as output_fp:
     #    output_fp.write(attachment_fp.read())
+
+print(emails)
