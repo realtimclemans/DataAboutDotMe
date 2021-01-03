61da14944e6d05e94cbee871398710fa83d642f6
Tim Clemans <realtimclemans@gmail.com>
2021-01-03 12:48:58 -0800

    add gmail and jail roster stuff

---
+++ opendata_website/scripts/collectorbot.py
@@ -0,0 +1,35 @@
+records_request = """This is a public records request to every PRA covered-entity in Washington State (see https://realtimclemans.com/my-epic-records-requests-bot-plan-and-proposal-for-over-hauling-washington-states-out-dated-public-records-act/) made by CollectorBot on behalf of dataabout.me. The data will not be used for commercial purposes. It will be posted on an open data portal for free to the public without ads with a notice that it is illegal to use dashcam and list of persons for commercial purposes.
+ 
+This is one bot records request in a 24 hour period. Because I am a bot I can not use a portal such as GovQA and demand all responses be in text format not PDF/Word and that records be disclosed as zip files via email attachment (not GovQA attachment) or direct link to the zip file that doesn't require logging in
+ 
+I demand all records be electronically redacted. Absolutely do not print out or scan anything.
+ 
+For yesterday the police/FIRE/EMS 911 text log, dispatch events, remarks, and individual unit dispatch tables exported to CSV with redacted strings replaced by "*R*"
+For yesterday all police reports, accident reports, citations from all departments including code enforcement and law enforcement
+Active warrants list exported to CSV with redacted strings replaced by "*R*" and photos with identifier to tied to the textual data
+Complete adult jail bookings related database tables exported to CSV with redacted strings replaced by "*R*"
+All already-electronic investigative records involving Timothy Clemans or 11448 12th ave sw Burien WA, 98146
+All already-electronic investigative records for all swatting, DUIs, violent crimes, child abuse, and sexual crimes, police chases that occured on or after December 29, 2020
+Yesterday's misconduct investigative records for all departments
+The IAPro SQL database exported to CSV with redacted strings replaced by "*R*"
+Yesterday's public records requests, communications with meta-data about records requests, and released records
+SMS/text messages with meta-data of all executive staff and PIOs (see https://www.ecamm.com/mac/phoneview/ and https://www.nickshertzer.com/?p=1768) to CSV with redacted strings replaced by "*R*"
+All listings of employee data and associated listings
+All automatic license plate reader textual data exported to CSV with redacted strings replaced by "*R*"
+All internet history logs exported to CSV with redacted strings replaced by "*R*"
+All records indexes
+All policies and training documents
+All business license records in a SQL database exported to CSV with redacted strings replaced by "*R*"
+All ID badges from the ID badge database and all electronic photos with meta-data of all employees
+All SQL database tables with employee data exported to CSV with redacted strings replaced by "*R*"
+Meta-data for every email exported to CSV with redacted strings replaced by "*R*"
+Yesterday's records for all building permit requests
+All real estate SQL data exported to CSV with redacted strings replaced by "*R*"
+Yesterday's emails with meta-data to and from PIOs
+The marriage records database exported to CSV with redacted strings replaced by "*R*"
+All voter registration SQL data exported to CSV with redacted strings replaced by "*R*"
+AXON evidence created log and AXON evidence sharing log exported to CSV with redacted strings replaced by "*R*"
+Every email with meta-data about CollectorBot or Collector Bot or Timothy Clemans or Tim Clemans or DataAbout.me and NCIC/ACCESS data and audit trail for queries of Timothy Clemans DOB 7/19/1990.
+No exemption log is wanted to reduce the effort involved in fulfilling the request.
+
+"""
